aa57c5b5cf03cce1df85f556551b308d3f648829
Vladislav ZHereb'ev <111141693+redrepublics@users.noreply.github.com>
2026-06-15 16:13:23 +0300

    #pytest примеры

--- pytest_demo/pytest_demo_1.py
+++ pytest_demo/pytest_demo_1.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'https://official-joke-api.appspot.com/random_joke'
 
-
+#
 # get, парсинг ответа, сравнение переменных, статус
 # {"type":"general","setup":"How do you fix a damaged jack-o-lantern?","punchline":"You use a pumpkin patch.","id":123}
 def test_one():
